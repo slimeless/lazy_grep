b4903e6c78718de883e83f05f4c19ee25f13ac04
scooby <stopcrybby@gmail.com>
2024-09-30 18:52:10 +0300

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,9 @@ use view::display;
 
 #[derive(Parser)]
 struct Cli {
+    #[arg(short, long)]
     pattern: String,
+    #[arg(short, long)]
     #[clap(default_value = "-")]
     data: MaybeStdin<String>,
 }
@@ -20,17 +22,22 @@ struct Cli {
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Cli::parse();
     let binding = args.data.to_string();
-    let new_path = Path::new(&binding);
     let regex = Regex::new(&args.pattern).unwrap();
-    let matches = match new_path.get_argtype() {
-        ArgType::File => {
-            let file = File::open(new_path)?;
-            file.get_content(&regex)?
-        }
-        ArgType::Directory => new_path.get_content(&regex)?,
-        ArgType::Stdin => binding.get_content(&regex)?,
-    };
+    let matches = execute_grep(binding, &regex)?;
     display(matches, regex);
 
     Ok(())
+}
+
+pub fn execute_grep(binding: String, regex: &Regex) -> Result<Vec<String>, Box<dyn Error>> {
+    let new_path = Path::new(&binding);
+    let matches = match new_path.get_argtype() {
+        ArgType::File => {
+            let file = File::open(new_path)?;
+            file.get_content(regex)?
+        }
+        ArgType::Directory => new_path.get_content(regex)?,
+        ArgType::Stdin => binding.get_content(regex)?,
+    };
+    Ok(matches)
 }
