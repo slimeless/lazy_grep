f06423a616362a922395b5af26e52f76f6c72c35
scooby <stopcrybby@gmail.com>
2024-09-30 19:12:53 +0300

    now use main and lib together (cuz i want to write tests in tests/)

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+mod argtype;
+mod grep;
+pub mod view;
+
+use argtype::{ArgType, GetArgType};
+use grep::GetContent;
+use regex::Regex;
+use std::{error::Error, fs::File, path::Path};
+
+pub fn execute_grep(binding: String, regex: &Regex) -> Result<Vec<String>, Box<dyn Error>> {
+    let new_path = Path::new(&binding);
+    let matches = match new_path.get_argtype() {
+        ArgType::File => {
+            let file = File::open(new_path)?;
+            file.get_content(regex)?
+        }
+        ArgType::Directory => new_path.get_content(regex)?,
+        ArgType::Stdin => binding.get_content(regex)?,
+    };
+    Ok(matches)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
-mod argtype;
-mod grep;
-mod view;
-
-use argtype::{ArgType, GetArgType};
 use clap::Parser;
 use clap_stdin::MaybeStdin;
-use grep::GetContent;
 use regex::Regex;
-use std::{error::Error, fs::File, path::Path};
-use view::display;
+use rustgrep::execute_grep;
+use rustgrep::view::display;
+use std::error::Error;
 
 #[derive(Parser)]
 struct Cli {
@@ -27,17 +22,4 @@ fn main() -> Result<(), Box<dyn Error>> {
     display(matches, regex);
 
     Ok(())
-}
-
-pub fn execute_grep(binding: String, regex: &Regex) -> Result<Vec<String>, Box<dyn Error>> {
-    let new_path = Path::new(&binding);
-    let matches = match new_path.get_argtype() {
-        ArgType::File => {
-            let file = File::open(new_path)?;
-            file.get_content(regex)?
-        }
-        ArgType::Directory => new_path.get_content(regex)?,
-        ArgType::Stdin => binding.get_content(regex)?,
-    };
-    Ok(matches)
 }
