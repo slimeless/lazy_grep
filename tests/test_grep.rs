f0de7468a4f13b6717cf50c4e9fb500bb344fc14
scooby <stopcrybby@gmail.com>
2024-09-30 19:30:10 +0300

    added tests

---
+++ tests/test_grep.rs
@@ -0,0 +1,13 @@
+use std::error::Error;
+
+use regex::Regex;
+use rustgrep::execute_grep;
+
+#[test]
+fn test_email() -> Result<(), Box<dyn Error>> {
+    let re = Regex::new(r"(?i)^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")?;
+    let binding = "john.doe@example.com\nalice.smith123@sub.domain.com\nuser+tag@domain.co.uk\nfirst_last@domain.org\nname@domain123.com\n\ninvalid-email@.com\nuser@domain..com\n@missingusername.com\nusername@domain.c\nuser@domain@another.com\n";
+    let result = execute_grep(binding.to_string(), &re)?;
+    assert_eq!(result.len(), 6);
+    Ok(())
+}
